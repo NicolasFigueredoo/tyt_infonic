f269950bf06f979dc247036407820e1caaa179b5
Nicolas Figueredo <gtyt@outlook.com.ar>
2026-05-13 16:22:48 -0300

    n

--- app/Http/Controllers/LoginClienteController.php
+++ app/Http/Controllers/LoginClienteController.php
@@ -629,7 +629,6 @@ return redirect()->back()
                     'dcamacho.tytsa@gmail.com',
                     'lmorales.tytsa@gmail.com',
                     'ariel@tytsa.com.ar',
-                    'nicolasfigueredo_02@hotmail.com'
                 ])
                 ->send($email);
 
