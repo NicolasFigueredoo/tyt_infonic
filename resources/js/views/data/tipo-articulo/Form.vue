0ca9c233b0c165ea9d3fc1b98c73b7b26fc3d285
Nicolas Figueredo <gtyt@outlook.com.ar>
2026-05-20 14:33:37 -0300

    b

--- resources/js/views/data/tipo-articulo/Form.vue
+++ resources/js/views/data/tipo-articulo/Form.vue
@@ -1,4 +1,3 @@
-
 <template>
     <div class="grid-3 gap-15">
         <InputText
@@ -31,96 +30,130 @@
             :error="errors.code"
             class="col-3"
         />
-        
+
         <div class="col-3">
+            <template v-if="!form.principal">
+                <label for="categoria">Categoría: </label>
+                <select
+                    v-model="form.sub_categoria_id"
+                    :error="errors.sub_categoria_id"
+                    class="form-select"
+                    id="categoria"
+                >
+                    <option value="" disabled>Seleccione una categoría</option>
+                    <option
+                        v-for="categoria in form.categorias"
+                        :key="categoria.id"
+                        :value="categoria.id"
+                    >
+                        {{ categoria.name }}
+                    </option>
+                </select>
 
-<template v-if="!form.principal">
+                <small v-if="errors.categoria" class="text-danger">{{
+                    errors.categoria
+                }}</small>
+            </template>
 
-    <label for="categoria">Categoría: </label>
-    <select v-model="form.sub_categoria_id" :error="errors.sub_categoria_id" class="form-select" id="categoria">
-
-      <option value="" disabled>Seleccione una categoría</option>
-      <option v-for="categoria in form.categorias" :key="categoria.id" :value="categoria.id">
-        {{ categoria.name }}
-      </option>
-
-
-    </select>
-
-    
-    <small v-if="errors.categoria" class="text-danger">{{ errors.categoria }}</small>
-</template>
-
-
-<template v-if="!form.principal">
-
-
-    <div class="col-12" style="margin-top: 20px">
-        <label for="productos">Productos: </label>
-        <multiselect 
-            v-model="form.seleccionados" 
-            :options="form.productos" 
-            track-by="id" 
-            label="name" 
-            placeholder="Selecciona productos" 
-            multiple 
-            id="productos"
-            :close-on-select="false" 
-            :preserve-search="true">
-        </multiselect>
-    </div>
-</template>
-
-
-
-    </div>
+            <template v-if="!form.principal">
+                <div class="col-12" style="margin-top: 20px">
+                    <label for="productos">Productos: </label>
+                    <multiselect
+                        v-model="form.seleccionados"
+                        :options="form.productos"
+                        track-by="id"
+                        label="name"
+                        placeholder="Selecciona productos"
+                        multiple
+                        id="productos"
+                        :close-on-select="false"
+                        :preserve-search="true"
+                    >
+                    </multiselect>
+                </div>
+            </template>
+        </div>
         <InputFileImage
-                    label="Marca"
-                    placeholder=""
-                    v-model="form.imagenMarca"
-                    :error="errors.imagenMarca"
-                    class="col-1"
+            label="Marca"
+            placeholder=""
+            v-model="form.imagenMarca"
+            :error="errors.imagenMarca"
+            class="col-1"
         />
-        <small  class="col-3">Tama&ntilde;o recomendado 16 x 200 px</small>
-        <img v-bind:src="form.imagenMarca" class="col-3" onerror="this.onerror=null;">
+        <small class="col-3">Tama&ntilde;o recomendado 16 x 200 px</small>
+        <img
+            v-bind:src="form.imagenMarca"
+            class="col-3"
+            onerror="this.onerror=null;"
+        />
 
         <InputFileImage
-                    label="Image"
-                    placeholder=""
-                    v-model="form.imagen"
-                    :error="errors.imagen"
-                    class="col-1"
+            label="Image"
+            placeholder=""
+            v-model="form.imagen"
+            :error="errors.imagen"
+            class="col-1"
         />
-        <small  class="col-3">Tama&ntilde;o recomendado 200 x 200 px</small>
-        <img v-bind:src="form.imagenSrc" class="col-3" onerror="this.onerror=null;">
+        <button
+            v-if="form.imagenSrc"
+            type="button"
+            class="btn btn-sm btn-outline-danger"
+            @click="
+                form.imagen = 0;
+                form.imagenSrc = null;
+            "
+            title="Quitar imagen"
+        >
+             Eliminar imagen
+        </button>
+
+        <small class="col-3">Tama&ntilde;o recomendado 200 x 200 px</small>
+        <img
+            v-bind:src="form.imagenSrc"
+            class="col-3"
+            onerror="this.onerror=null;"
+        />
 
         <div class="form-check">
-            <input class="form-check-input" v-model="form.oculto" :error="errors.oculto" type="checkbox" id="oculto">
+            <input
+                class="form-check-input"
+                v-model="form.oculto"
+                :error="errors.oculto"
+                type="checkbox"
+                id="oculto"
+            />
             <label class="form-check-label" for="oculto">
                 Ocultar categoria
             </label>
         </div>
         <div class="form-check">
-            <input class="form-check-input" v-model="form.destacado" :error="errors.destacado" type="checkbox" id="destacado">
-            <label class="form-check-label" for="destacado">
-                Destacado
-            </label>
+            <input
+                class="form-check-input"
+                v-model="form.destacado"
+                :error="errors.destacado"
+                type="checkbox"
+                id="destacado"
+            />
+            <label class="form-check-label" for="destacado"> Destacado </label>
         </div>
 
         <div class="form-check">
-            <input class="form-check-input" v-model="form.principal" :error="errors.principal" type="checkbox" id="principal">
-            <label class="form-check-label" for="principal">
-                Principal
-            </label>
+            <input
+                class="form-check-input"
+                v-model="form.principal"
+                :error="errors.principal"
+                type="checkbox"
+                id="principal"
+            />
+            <label class="form-check-label" for="principal"> Principal </label>
         </div>
     </div>
 </template>
 
-
 <script setup>
 import { reactive } from "@vue/reactivity";
-import { defineProps } from 'vue';
-import Multiselect from 'vue-multiselect';
+import { defineProps } from "vue";
+import Multiselect from "vue-multiselect";
 
 // Definición de propiedades
 const props = defineProps({
@@ -136,15 +169,14 @@ const props = defineProps({
 
 // Opciones para el select
 const options = reactive([
-    { id: 1, name: 'Producto 1' },
-    { id: 2, name: 'Producto 2' },
-    { id: 3, name: 'Producto 3' },
-    { id: 4, name: 'Producto 4' },
-    { id: 5, name: 'Producto 5' },
+    { id: 1, name: "Producto 1" },
+    { id: 2, name: "Producto 2" },
+    { id: 3, name: "Producto 3" },
+    { id: 4, name: "Producto 4" },
+    { id: 5, name: "Producto 5" },
 ]);
-
 </script>
 
 <style lang="scss" scoped>
-@import 'vue-multiselect/dist/vue-multiselect.min.css';
+@import "vue-multiselect/dist/vue-multiselect.min.css";
 </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,84 @@
-<!DOCTYPE html><html lang="{{ str_replace('_', '-', app()->getLocale()) }}"><head>    <meta charset="utf-8">    <meta name="viewport" content="width=device-width, initial-scale=1">    <title>TyT</title>    <meta name="public-path" content="{{ asset('/') }}">    <meta name="base-url" content="{{ request()->getBaseUrl() }}">    <meta name="storage-path" content="{{ asset(Storage::url('/')) }}">    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">    <link rel="stylesheet" href="{{ asset('css/app.css') }}">    <link rel="stylesheet" href="{{ asset('images/icons/fontello/css/hard.css') }}?13">    <link rel="stylesheet" href="https://cdn.quilljs.com/1.3.7/quill.snow.css"></head><body>    <div id="app">prueba</div>    <script src="{{ asset('js/app.js') }}?13"></script>    <!-- Agregar los archivos JS de Quill -->    <script src="https://cdn.quilljs.com/1.3.7/quill.js"></script>    <script>        // Inicializar Quill en el editor        var editor = new Quill('#editor', {            theme: 'snow'        });    </script></body></html>
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TyT</title>
+
+    <meta name="public-path" content="{{ asset('/') }}">
+
+    <meta name="base-url" content="{{ request()->getBaseUrl() }}">
+
+
+
+    <meta name="storage-path" content="{{ asset(Storage::url('/')) }}">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('images/icons/fontello/css/hard.css') }}?13">
+
+    <link rel="stylesheet" href="https://cdn.quilljs.com/1.3.7/quill.snow.css">
+
+
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-K45SRCK5');
+    </script>
+    <!-- End Google Tag Manager -->
+
+</head>
+
+
+
+<body>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K45SRCK5" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+    <div id="app">prueba</div>
+
+    <script src="{{ asset('js/app.js') }}?13"></script>
+
+    <!-- Agregar los archivos JS de Quill -->
+
+    <script src="https://cdn.quilljs.com/1.3.7/quill.js"></script>
+
+    <script>
+        // Inicializar Quill en el editor
+
+        var editor = new Quill('#editor', {
+
+            theme: 'snow'
+
+        });
+    </script>
+
+</body>
+
+
+
+</html>
